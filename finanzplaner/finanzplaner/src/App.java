bdf0b42e2474f5d41e5fe5e3a3f1c765e50cb7b4
IHannes <hannes.hildner@icloud.com>
2022-05-19 16:51:20 +0000

    initial commit - Main Application

---
+++ finanzplaner/finanzplaner/src/App.java
@@ -0,0 +1,8 @@
+//import javax.swing.*;
+//import java.awt.*;
+public class App {
+    public static void main(String[] args) throws Exception {
+        int lan = lang.lang();
+        new currencyframe(lan);
+    }
+}
